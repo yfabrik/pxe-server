62bcaba81941b48f6b49065938e9d30e15f7c0d7
yfabrik <138105766+yfabrik@users.noreply.github.com>
2025-02-12 15:29:46 +0100

    ipxe tweak

--- docker/pihole/build.sh
+++ docker/pihole/build.sh
@@ -13,6 +13,15 @@ wget https://github.com/ipxe/wimboot/releases/latest/download/wimboot
 echo "ipxe"
 git clone https://github.com/ipxe/ipxe.git
 cd ./ipxe/src || (echo "no such dir " && exit)
+
+echo "tweak ipxe"
+sed -i "/define POWEROFF_CMD/s|^//||" config/general.h
+sed -i "/define REBOOT_CMD/s|^//||" config/general.h
+sed -i "/define PING_CMD/s|^//||" config/general.h
+
+sed -i "/define KEYBOARD_MAP/s/\s\+\S\+$/ fr/" config/console.h
+echo "tweak done"
+
 make bin/undionly.kpxe
 make bin-x86_64-efi/ipxe.efi
 make bin-x86_64-efi/snponly.efi
